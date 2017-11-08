4c918c8a67cd32a36122a80fcd27a9139ad99c75
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-11-08 11:17:35 -0500

    Feature: Add elastic collision resolve function

--- app.js
+++ app.js
@@ -7,8 +7,55 @@ const getDistance = (x1, y1, x2, y2) => {
   return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
 
+const rotate = (ball, angle) => {
+    const rotatedVelocities = {
+        dx: ball.dx * Math.cos(angle) - ball.dy * Math.sin(angle),
+        dy: ball.dx * Math.sin(angle) + ball.dy * Math.cos(angle)
+    };
+    return rotatedVelocities;
+}
+
+const resolveCollision = (particle, otherParticle) => {
+    const xVelocityDiff = particle.dx - otherParticle.dx;
+    const yVelocityDiff = particle.dy - otherParticle.dy;
+
+    const xDist = otherParticle.x - particle.x;
+    const yDist = otherParticle.y - particle.y;
+
+    // Prevent accidental overlap of particles
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+        // Grab angle between the two colliding particles
+        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+        // Store mass in var for better readability in collision equation
+        const m1 = particle.mass;
+        const m2 = otherParticle.mass;
+
+        // Velocity before equation
+        const u1 = rotate(particle, angle);
+        const u2 = rotate(otherParticle, angle);
+
+        // Velocity after 1d collision equation
+        const v1 = { dx: u1.dx * (m1 - m2) / (m1 + m2) + u2.dx * 2 * m2 / (m1 + m2), dy: u1.dy };
+        const v2 = { dx: u2.dx * (m1 - m2) / (m1 + m2) + u1.dx * 2 * m2 / (m1 + m2), dy: u2.dy };
+
+        // Final velocity after rotating axis back to original location
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+
+        // Swap particle velocities for realistic bounce effect
+        particle.dx = vFinal1.dx;
+        particle.dy = vFinal1.dy;
+
+        otherParticle.dx = vFinal2.dx;
+        otherParticle.dy = vFinal2.dy;
+    }
+}
+
 class Circle {
   constructor(x, y, radius) {
+    this.mass = 1;
     this.x = x;
     this.y = y;
     this.dx = (Math.random() - 0.5) * 10;
@@ -25,6 +72,7 @@ class Circle {
     for (let i = 0; i < allCircles.length; i += 1) {
       if (this === allCircles[i]) continue;
       if ((getDistance(this.x, this.y, allCircles[i].x, allCircles[i].y) - this.radius - allCircles[i].radius) < 0) {
+        resolveCollision(this, allCircles[i]);
       }
     }
     if (this.x > canvas.width - this.radius || this.x - this.radius < 0) {
