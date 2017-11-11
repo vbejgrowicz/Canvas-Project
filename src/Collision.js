58db7b17043abfbc6a9b24a5bde67a9953e391f3
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-11-11 12:30:32 -0500

    Refactor: Refactor collision logic

--- src/Collision.js
+++ src/Collision.js
@@ -8,9 +8,7 @@ const Collision = {
   },
   getDistance(ballOne, ballTwo){
     let xDistance = ballTwo.x - ballOne.x;
-    // console.log(xDistance);
     let yDistance = ballTwo.y - ballOne.y;
-    // console.log(yDistance);
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
   },
   resolveCollision(ballOne, ballTwo) {
@@ -18,73 +16,49 @@ const Collision = {
     const vx2i = ballTwo.dx;
     const vy1i = ballOne.dy;
     const vy2i = ballTwo.dy;
-    // debugger;
-    // console.log('One', ballOne.dx);
-    // console.log('Two', ballTwo.dx);
+
     // Initial Velocitys = Final Velocitys
     //Elastic Collisions
     //v1i + v1f = v2i + v2f
 
     // X Direction
-    const elasticEqX = algebra.parse(`${vx1i} + vx1f = ${vx2i} + vx2f`);
-    const SolveForX1f = elasticEqX.solveFor("vx1f");
-    // console.log(SolveForX1f.toString())
+    const elasticEqSolvedforX1f = (`${vx2i} + vx2f - ${vx1i}`);
 
     //Momentum Equation
     //m1 * v1i + m2 * v2i = m1 * v1f + m2 * v2f
     const mx1i = ballOne.mass * ballOne.dx;
     const mx2i = ballTwo.mass * ballTwo.dx;
-    const mxi = this.roundtoTwoDec(mx1i + mx2i);
+    const mxi = mx1i + mx2i;
 
-    // console.log(`${mxi} = ${ballOne.mass} * ${SolveForX1f} + ${ballTwo.mass} * vx2f `)
+    const momentumEqX = algebra.parse(`${mxi} = ${ballOne.mass} * (${vx2i} + vx2f - ${vx1i}) + ${ballTwo.mass} * vx2f`);
+    const vx2fAns = momentumEqX.solveFor("vx2f");
 
-
-    const momentumEqX = algebra.parse(`${mxi} = ${ballOne.mass} * ${SolveForX1f} + ${ballTwo.mass} * vx2f `);
-    const SolveForX2f = momentumEqX.solveFor("vx2f");
-    // console.log(SolveForX2f.toString())
-    const vx2f = this.roundtoTwoDec(SolveForX2f.numer / SolveForX2f.denom);
+    let vx2f = this.roundtoTwoDec(vx2fAns.numer / vx2fAns.denom);
 
     //Plug vx2f back into first Equation
-    const vx2 = this.roundtoTwoDec(vx2i + vx2f);
-    const elasticEqX1 = algebra.parse(`${vx1i} + vx1f = ${vx2}`);
-    const ResultX1f = elasticEqX1.solveFor("vx1f");
-    const vx1f = this.roundtoTwoDec(ResultX1f.numer / ResultX1f.denom);
+    let vx1f = this.roundtoTwoDec(vx2i + vx2f - vx1i);
 
     // Y Direction
-    const elasticEqY = algebra.parse(`${vy1i} + vy1f = ${vy2i} + vy2f`);
-    const SolveForY1f = elasticEqY.solveFor("vy1f");
+    const elasticEqSolvedforY1f = (`${vy2i} + vy2f - ${vy1i}`);
 
     //Momentum Equation
     //m1 * v1i + m2 * v2i = m1 * v1f + m2 * v2f
     const my1i = ballOne.mass * ballOne.dy;
     const my2i = ballTwo.mass * ballTwo.dy;
-    const myi = this.roundtoTwoDec(my1i + my2i);
+    const myi = my1i + my2i;
 
-    const momentumEqY = algebra.parse(`${myi} = ${ballOne.mass} * ${SolveForY1f} + ${ballTwo.mass} * vy2f `);
-    const SolveForY2f = momentumEqY.solveFor("vy2f");
-    const vy2f = this.roundtoTwoDec(SolveForY2f.numer / SolveForY2f.denom);
+    const momentumEqY = algebra.parse(`${myi} = ${ballOne.mass} * (${vy2i} + vy2f - ${vy1i}) + ${ballTwo.mass} * vy2f`);
+    const vy2fAns = momentumEqY.solveFor("vy2f");
+
+    let vy2f = this.roundtoTwoDec(vy2fAns.numer / vy2fAns.denom);
 
     //Plug vx2f back into first Equation
-    const vy2 = this.roundtoTwoDec(vy2i + vy2f);
-    const elasticEqY1 = algebra.parse(`${vy1i} + vy1f = ${vy2}`);
-    const ResultY1f = elasticEqY1.solveFor("vy1f");
-    const vy1f = this.roundtoTwoDec(ResultY1f.numer / ResultY1f.denom);
-    // console.log(ballOne.dx, ballOne.dy)
-    // console.log(ballTwo.dx, ballTwo.dy)
-    
     ballOne.dx = Math.abs(vx1f) > 10 ? ((vx1f / Math.abs(vx1f)) * 10) : vx1f;
     ballOne.dy = Math.abs(vy1f) > 10 ? ((vy1f / Math.abs(vy1f)) * 10) : vy1f;
     ballTwo.dx = Math.abs(vx2f) > 10 ? ((vx2f / Math.abs(vx2f)) * 10) : vx2f;
     ballTwo.dy = Math.abs(vy2f) > 10 ? ((vy2f / Math.abs(vy2f)) * 10) : vy2f;
+    let vy1f = this.roundtoTwoDec(vy2i + vy2f - vy1i);
 
-    // console.log(ballOne.dx, ballOne.dy, ballTwo.dx, ballTwo.dy)
-    console.log(ballOne.dx);
-    console.log(ballOne.dy);
-    console.log(ballTwo.dx);
-    console.log(ballTwo.dy);
-    // console.log(vx1f, vx2f, vy1f, vy2f);
-    // console.log(ballOne.dx, ballOne.dy)
-    // console.log(ballTwo.dx, ballTwo.dy)
   },
   checkCollision(ballOne, ballTwo, currentCollisions) {
       if ((this.getDistance(ballOne, ballTwo) - ballOne.radius - ballTwo.radius) < 0) {
